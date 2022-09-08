0153871f245bb7ce2b3950567136f9676217a34d
Babatunde David <davidbabs19@yahoo.com>
2022-09-08 14:21:10 -0700

    All commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,23 +7,16 @@
  */
 int main(void)
 {
-	char a;
-	
-	int b;
-	
-	long int c;
-	
-	long long int d;
-	
-	float e;
+	char a = '1';
+	int b = 4;
+	long int c = 4;
+	long long int d = 8;
+	float e = 4;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	
-	printf("Size of an int: %d byte(s)\n", sizeof(b));
-	
-	printf("Size of a long int: %d byte(s)\n", sizeof(d));
-	
-	printf("Size of a float: %d byte(s)\n", sizeof(e));
-
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of an int: %d byte(s)\n", b);
+	printf("Size of a long int: %d byte(s)\n", c);
+	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size of a float: %f byte(s)\n", e);
 	return (0);
 }
