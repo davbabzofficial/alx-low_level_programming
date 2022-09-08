ae6f8dd511c3d52974f636ce2a74e989e878c361
Babatunde David <davidbabs19@yahoo.com>
2022-09-08 13:28:21 -0700

    Davbabz

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Printing
  *
- * Retrun: Always 0 (Successful)
+ * Retrun: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
