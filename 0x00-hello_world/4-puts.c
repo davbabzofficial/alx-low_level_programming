b15fe813e8c8e74d04498ce387df25cf3be9d340
Babatunde David <davidbabs19@yahoo.com>
2022-09-08 12:05:45 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main -Printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	Puts("\"Programming is like building a muiltilingual puzzle");
+	return (0);
+}
