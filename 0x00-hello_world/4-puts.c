f79cf3afa9f301e5904bb5adf37557b5fe8eff2a
Babatunde David <davidbabs19@yahoo.com>
2022-09-08 13:16:20 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-int main ()
+#include<stdio.h>
+/**
+ * main - Printing
+ *
+ * Retrun: Always 0 (Successful)
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
