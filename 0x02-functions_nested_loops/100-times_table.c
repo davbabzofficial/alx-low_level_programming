8cd5e79929e77c39f68a4bac25dfa8d854c143af
Babatunde David <davidbabs19@yahoo.com>
2022-09-14 21:15:50 +0000

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,15 +26,15 @@ void print_times_table(int n)
 				_putchar (' ');
 			if (prod >= 100)
 			{
-			_putchar ((prod / 100) + '0');
-			_putchar (((prod / 10)) % 10 + '0');
+			_putcha((prod / 100) + '0');
+			_putchar(((prod / 10)) % 10 + '0');
 			}
 			else if
 (prod <= 99 && prod >= 10)
 			{
-			_putchar ((prod / 10) + '0');
+			_putchar((prod / 10) + '0');
 			}
-			_putchar ((prod % 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
