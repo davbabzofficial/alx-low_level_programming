feb105374c07ebe88f45f82269ee848240140c7a
Babatunde David <davidbabs19@yahoo.com>
2022-09-20 14:37:19 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* puts - prints every other character
+* @str: string
+*/
+
+void puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
