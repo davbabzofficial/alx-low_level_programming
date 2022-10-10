58a891532b71d86de9316aa84195c95644d67c21
Babatunde David <davidbabs19@yahoo.com>
2022-10-10 16:09:28 +0000

    Task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,7 +6,7 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-char *argstrostr(int ac, char **av);
+char *argstostr(int ac, char **av);
 int _strlen(char *c);
 
 #endif /* MAIN_H */
