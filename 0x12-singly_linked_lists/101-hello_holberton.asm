c1c89166e10473d01347a192e7600319dc5367b8
Babatunde David <davidbabs19@yahoo.com>
2022-10-22 20:40:42 +0000

    Task 3-5

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,10 @@
-SECTION .data
-msg:		db "Hello Holberton", 0
-fmt:		db "%s", 10 0
-
-	SECTION .TEXT
-	extern printf
+	section .text
 	global main
-
+	extern printf
 main:
-	mov esi, mgs
-	mov edi, fmt
-	mov eax, 0
-	call printf
+	mov	edi, mess
+	mov	eax, 0
+	call	printf
 
-	mov eax, 0
-	ret
+	section .data
+mess:db		"Hello, Holberton", 10
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,18 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
-* free_list - frees a list
-* @head: head of the linked list.
-* Return: no return.
+* free_list - function that frees a list_t list
+* @head: pointer to list_t list
 */
 void free_list(list_t *head)
 {
-	list_t *current;
+	list_t *temp;
 
-	while ((current = head) != NULL)
+	while (head)
 	{
-		head = head->next;
-		free(current->str);
-		free(current);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
